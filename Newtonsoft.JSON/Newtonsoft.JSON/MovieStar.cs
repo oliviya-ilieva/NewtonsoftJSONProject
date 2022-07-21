27e28fe1b6052604ebb89bfe966dba947e9191e2
Oliviya P. Ilieva <59021880+oliviya-ilieva@users.noreply.github.com>
2022-07-21 21:55:15 +0300

    Add files via upload

---
+++ Newtonsoft.JSON/Newtonsoft.JSON/MovieStar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newtonsoft.JSON
+{
+    [Serializable]
+    public class MovieStar
+    {
+
+
+        public DateTime dateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+
+       
+    }
+}
---
+++ Newtonsoft.JSON/Newtonsoft.JSON/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Newtonsoft.JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var jsonText = File.ReadAllText(@"C:\Users\PC\OneDrive\Desktop\newtonsoft\input.txt");
+            var movieStars = JsonConvert.DeserializeObject<IList<MovieStar>>(jsonText);
+
+            foreach (var movieStar in movieStars)
+            {
+                Console.WriteLine("{0} {1}", movieStar.Name, movieStar.Surname);
+                Console.WriteLine(movieStar.Sex);
+                Console.WriteLine(movieStar.Nationality);
+
+                // We use method to get the age of the user. Can be implemented in another way.
+                int age = new DateTime(DateTime.Now.Subtract(movieStar.dateOfBirth).Ticks).Year - 1;
+                Console.WriteLine(age + " years old");
+                Console.WriteLine();
+
+
+            }
+             }
+    }
+}
